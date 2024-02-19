1a0960d0e0ae38f9f7741acb9690258d74045d5f
pariaspe <pedro.ariasp@upm.es>
2024-02-19 17:32:23 +0100

    add gazebo launcher

---
+++ launch/gazebo.launch.py
@@ -0,0 +1,81 @@
+"""
+gazebo.launch.py
+"""
+
+# Copyright 2024 Universidad Politécnica de Madrid
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+__authors__ = "Pedro Arias Pérez"
+__copyright__ = "Copyright (c) 2024 Universidad Politécnica de Madrid"
+__license__ = "BSD-3-Clause"
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription, LaunchContext
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+
+
+def launch_simulation(context: LaunchContext):
+    """Launch the simulation
+    """
+    verbose = LaunchConfiguration('verbose').perform(context)
+    verbose = verbose.lower() in ['true', 't', 'yes', 'y', '1']
+    verbose = '--verbose' if verbose else ''
+
+    world_path = LaunchConfiguration('world').perform(context)
+    gazebo11 = ExecuteProcess(
+        cmd=['gazebo', verbose, world_path],
+        name='gazebo11',
+        output='screen',
+    )
+    return [gazebo11]
+
+
+def generate_launch_description():
+    """Generate Launch description with GzSim launch + Models Spawning + World/Object bridges
+    """
+    world_path = os.path.join(get_package_share_directory(
+        'ros_follow_line'), 'worlds', 'simple_circuit.world')
+
+    return LaunchDescription([
+        # Launch Arguments
+        DeclareLaunchArgument(
+            'verbose',
+            default_value='false',
+            choices=['true', 'false'],
+            description='Launch in verbose mode.'),
+        DeclareLaunchArgument(
+            'world',
+            default_value=world_path,
+            description='Path to world file.'),
+        # Launch processes
+        OpaqueFunction(function=launch_simulation)
+    ])
